f2f504d83643fb44afd94d5c00b6c7c2585c7a9d
Xyrephon <jtuc1613@740s-pl-115.local>
2016-09-14 09:00:00 -0600

    Made the monster class Constructor.

---
+++ src/monster/model/MarshmallowMonster.java
@@ -0,0 +1,40 @@
+package monster.model;
+
+public class MarshmallowMonster
+{
+
+	private String name;
+	private double antennaCount;
+	private int eyeCount;
+	private boolean hasBellyButton;
+	private int armCount;
+	private int noseCount;
+	
+	public MarshmallowMonster()
+	{
+		this.name = "no name here";
+		this.antennaCount = -23423.131235;
+		this.eyeCount = -12345678;
+		this.hasBellyButton = false;
+		this.armCount = -32436;
+		this.noseCount = -7765;
+	}
+	
+	public MarshmallowMonster(String name, double antennaCount, int eyeCount, boolean hasBellyButton, int armCount, int noseCount)
+	{
+		this.name = name;
+		this.antennaCount = antennaCount;
+		this.eyeCount = eyeCount;
+		this.hasBellyButton = hasBellyButton;
+		this.armCount = armCount;
+		this.noseCount = noseCount;
+	}
+	/* 
+	 * name = Hairy Jerry III
+	 * antennaCount = 2
+	 * eyeCount = 3
+	 * hasBellyButton = false
+	 * armCount = 6
+	 * noseCount = 1
+	 */
+}
