5dc527ae5b5d0e0b736999b41f1150a2180551ee
Xyrephon <tuckerjonah@gmail.com>
2016-10-26 21:50:48 -0600

    Added icon to the input window.

--- src/monster/view/MonsterView.java
+++ src/monster/view/MonsterView.java
@@ -1,6 +1,7 @@
 package monster.view;
 
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class MonsterView 
 {
@@ -11,6 +12,15 @@ public class MonsterView
 	{
 		windowMessage = "This message brought to you by the MonsterProject! :D";
 		monsterIcon = new ImageIcon(getClass().getResource("images/monsterproject.png"));
+	}
+	
+	public String collectUserText(String displayText)
+	{
+		String userInput = "";
+		
+		userInput = JOptionPane.showInputDialog(null, displayText, windowMessage, JOptionPane.INFORMATION_MESSAGE, monsterIcon, null, "Type here please!").toString();
+		
+		return userInput;
 	}
 	
 }
